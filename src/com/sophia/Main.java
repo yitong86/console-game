af261eadb3982e4b1e799d3e5dac409e9389ef9a
yitong86 <YOUR_EMAIL_ADDRESS>
2021-12-17 19:12:57 -0500

    Test

--- src/com/sophia/Main.java
+++ src/com/sophia/Main.java
@@ -3,42 +3,42 @@ package com.sophia;
 import java.util.*;
 
 public class Main {
-   static List<Integer> booksNumber = new ArrayList<>();
+    static List<Integer> booksNumber = new ArrayList<>();
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-   List<String> books = new ArrayList<>();
-   books.add("The Snowy Day");
-   books.add("Where the Wild Things Are");
-   books.add("The Old Truck");
-   books.add("Crown: An Ode to the Fresh Cut");
-   books.add("Julián Is A Mermaid");
-   // add element
-   books.add("Star");
-   System.out.println(books);
+        List<String> books = new ArrayList<>();
+        books.add("The Snowy Day");
+        books.add("Where the Wild Things Are");
+        books.add("The Old Truck");
+        books.add("Crown: An Ode to the Fresh Cut");
+        books.add("Julián Is A Mermaid");
+        // add element
+        books.add("Star");
+        System.out.println(books);
 
-   booksNumber.add(1);
-   booksNumber.add(2);
-   booksNumber.add(3);
-   booksNumber.add(4);
-   booksNumber.add(5);
-   booksNumber.add(6);
-   booksNumber.add(7);
-   System.out.println(booksNumber);
-   //remove element
-    booksNumber.remove(1);
+        booksNumber.add(1);
+        booksNumber.add(2);
+        booksNumber.add(3);
+        booksNumber.add(4);
+        booksNumber.add(5);
+        booksNumber.add(6);
+        booksNumber.add(7);
+        System.out.println(booksNumber);
+        //remove element
+        booksNumber.remove(1);
         System.out.println(booksNumber);
 
 
-   //Using Java for loops (NOT FOR EACH), loop over these and output each element on a new line.
-        for(int i = 0; i < books.size();i++) {
+        //Using Java for loops (NOT FOR EACH), loop over these and output each element on a new line.
+        for (int i = 0; i < books.size(); i++) {
             System.out.println(books.get(i));
 
         }
 
 
-
-        for (int i = 0 ;i < booksNumber.size();i++) {
+        for (int i = 0; i < booksNumber.size(); i++) {
             System.out.println(booksNumber.get(i));
 
         }
@@ -48,21 +48,13 @@ public class Main {
 
 
         int sum = 0;
-        for (int i =0;i< booksNumber.size();i++) {
+        for (int i = 0; i < booksNumber.size(); i++) {
             sum += booksNumber.get(i);
 
         }
         System.out.println(sum);
-        }
-
-
-
-
-
-
-
+    }
 }
-//just test
 
 //      // when user input 100 then stop.otherwise keep going to enter number,use while loop.
 ////            Scanner scanner = new Scanner(System.in);
